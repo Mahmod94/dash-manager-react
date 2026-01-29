5c3d5fae8d189e741b3f5b733cbb063bde4a6f5a
Mahmod94 <78415546+Mahmod94@users.noreply.github.com>
2026-01-29 08:56:02 +0100

    useState
    deeper knowledge in usestates -- add edit delete

--- sort/src/App.tsx
+++ sort/src/App.tsx
@@ -1,30 +1,68 @@
 import { useState } from "react";
 
+type User = {
+  id: number;
+  name: string;
+}
+
 
 export default function App()
 {
-  type User = {
-    id: number;
-    name: string;
-    age: number;
-  }
-
-  const u: User[] = [
-    { id: 1, name: "Sam", age: 23},
-    { id: 2, name: "Sara", age: 15},
-    { id: 3, name: "Salwa", age: 34}
-  ]
-
-
   const [users, setUser] = useState<User[]>([]);
+  const [name, setName] = useState<string>("");
 
-  
+  const [fname, setFName] = useState<string>("");
+  const [fruits, setFruit] = useState<string[]>([]);
 
+  function handleAdd(){
+    if (!name.trim()) return;
 
+    const newUser: User = {
+      id: Date.now(),
+      name,
+    };
 
-  return (
+    setUser(prev => [...prev, newUser]);
+    setName("");
+  };
+
+  const addFruit = () => {
+    if (!fname.trim()) return;
+
+    setFruit(prev => [...prev, fname]);
+    setFName("");
+  };
+
+  const handleDelete = (id: number) => {
+    setUser(prev => prev.filter(u => u.id !== id));
+  };
+
+  const handleDeleteF = (fruit : string) => {
+    setFruit(prev => prev.filter(f => f !== fruit));
+  };
+
+  return(
     <>
+    <div>
+      <h2>Users</h2>
+      <input type="text" value={name} onChange={e => setName(e.target.value)}/>
+      <button onClick={handleAdd}>add</button>
+      <ul>
+        {users.map((u) => (
+          <li key={u.id}>{u.name}{" "} <button onClick={() => handleDelete(u.id)}>delete</button></li>
+        ))}
+      </ul>
 
+      <h2>Fruits</h2>
+      <input type="text" value={fname} onChange={e => setFName(e.target.value)} />
+      <button onClick={addFruit}>add</button>
+      <ul>
+        {fruits.map((f) => (
+          <li key={f}>{f}{" "} <button onClick={() => handleDeleteF(f)}>delete</button></li>
+        ))}
+      </ul>
+    </div>
+    
     </>
   );
 
