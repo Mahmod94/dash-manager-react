a1802c7a57eced7af4a82b30b02154ffaaa13992
Mahmod94 <78415546+Mahmod94@users.noreply.github.com>
2026-01-29 08:57:32 +0100

    Update users.ts

--- sort/src/assets/users.ts
+++ sort/src/assets/users.ts
@@ -2,7 +2,12 @@ type User = {
     id: number;
     name: string;
     age: number;
-}
+};
 
 
-const result = users.filter(u => u.name.startsWith("A"));
+const result: User[] = [
+    { id: 1, name: "Samuel", age: 23 },
+    { id: 2, name: "Sara", age: 30},
+    { id: 3, name: "Adam", age: 18},
+];
+
